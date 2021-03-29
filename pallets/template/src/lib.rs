50a1a924a1979bdd067de8798ef8a532bc33f04b
Плужникова Дарья <pluzhnikovadari@mail.ru>
2021-03-29 16:03:06 +0300

    public

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -66,7 +66,7 @@ decl_module! {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
+		pub fn do_something(origin = T::Origin, something: u32) -> dispatch::DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
 			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
